c1a988d1e888d860169331346938e97062365c68
Natyka Callwood <Natykacallwood@pursuit.org>
2023-07-12 19:24:37 -0400

    Updated the catz.html and catzApp.js files. To the catz.html file, added a 'h4' with an id of 'thank-you' to hold a message to be displayed once the form was submitted. Added a p tag with an id of 'random-fact-submit' to hold the random cat fact once the form is submitted. To the catzApp.js file added code so that when the page loads four random cat facts are fetched from the API and diplayed above each div element. Added code so that once the form is filled out, a 'thank you' message appears for signing up along with a random cat fact that is fetched from the API. Still need to add the error messages for when the form is not filled out correctly.

--- catzApp.js
+++ catzApp.js
@@ -1,9 +1,9 @@
 //   catzApp.js file //
 
 //Collection of 5 random cat facts - Was able to get the data
-fetch("https://cat-fact.herokuapp.com/facts")
-    .then((response) => response.json())
-    .then((JSONresponse) => console.log(JSONresponse))
+// fetch("https://cat-fact.herokuapp.com/facts")
+//     .then((response) => response.json())
+//     .then((JSONresponse) => console.log(JSONresponse))
 
 
 //Collection of 90 random cat facts - Was able to get data
@@ -12,11 +12,33 @@ fetch("https://meowfacts.herokuapp.com/?count=90")
     .then((response) => response.json())
     .then((JSONresponse) => {
         const catFacts = JSONresponse.data
-        console.log(catFacts)
-        const catFactOne = document.querySelector("#cat-fact-one")
-        const firstFetchedFact = document.querySelector("#fetch-one")
-        firstFetchedFact.innerText = catFacts[3]
-        catFactOne.append(firstFetchedFact)
+        // console.log(catFacts)
+        const catFactOne = document.querySelector("#cat-fact-one");
+        const firstFetchedFact = document.querySelector("#fetch-one");
+            firstFetchedFact.innerText = catFacts[5];
+            catFactOne.append(firstFetchedFact);
+        const catFactTwo = document.querySelector("#cat-fact-two");
+        const secondFetchedFact = document.querySelector("#fetch-two");
+            secondFetchedFact.innerText = catFacts[2];
+            catFactTwo.append(secondFetchedFact);
+        const catFactThree = document.querySelector("#cat-fact-three");
+        const thirdFetchedFact = document.querySelector("#fetch-three");
+            thirdFetchedFact.innerText = catFacts[6];
+            catFactThree.append(thirdFetchedFact);
+        const catFactFour = document.querySelector("#cat-fact-four");
+        const fourthFetchedFact = document.querySelector("#fetch-four");
+            fourthFetchedFact.innerText = catFacts[27];
+            catFactFour.append(fourthFetchedFact);
 
-
+        const addFact = document.querySelector("#add-fact");
+            addFact.addEventListener("submit", (event)=>{
+                event.preventDefault();
+                const randomFactSubmit = document.querySelector("#random-fact-submit");
+                const thankYouMessage = document.querySelector("#thank-you");
+                randomFactSubmit.innerText = `❤️ Here's your random cat fact: ❤️\n  ${catFacts[4]}`;
+                thankYouMessage.innerText = "Thanks for signing up!!";
+                addFact.append(thankYouMessage,randomFactSubmit)
+                
+            });
+        
     });
